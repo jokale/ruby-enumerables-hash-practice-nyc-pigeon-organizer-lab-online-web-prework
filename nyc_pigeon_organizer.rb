b59cb814a2423218329e24db4b3c6ec2df883793
Joanna E K <kalemajoanna@gmail.com>
2019-11-11 11:19:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,59 @@
 def nyc_pigeon_organizer(data)
   result_class = {
     "Theo" => {
-      :colour => ["grey", "purple"]
+      color: ["purple", "grey"],
+      gender: ["male"],
+      lives: ["Subway"]
     },
-    "Peter Jr." =>{},
-    "Lucky" => {},
-    "Ms. K"=> {},
-    "Queenie" => {},
-    "Andrew" => {},
-    "Alex" => {}
-    
+    "Peter Jr." =>{
+      color: ["purple","grey"],
+      gender: ["male"],
+      lives: ["Library"]
+    },
+    "Lucky" => {
+      color: ["purple"],
+      gender: ["male"],
+      lives: ["Central Park"]
+    },
+    "Ms. K"=> {
+      color: ["grey","white"],
+      gender: ["female"],
+      lives: ["Central Park"]
+    },
+    "Queenie" => {
+      color: ["white","brown"],
+      gender: ["female"],
+      lives: ["Subway"]
+    },
+    "Andrew" => {
+      color: ["white"],
+      gender: ["male"],
+      lives: ["City Hall"]
+    },
+    "Alex" => {
+      color: ["white","brown"],
+      gender: ["male"],
+      lives: ["Central Park"]
+    }
   }
+   new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
 end
+
